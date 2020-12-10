b39d97d39aacf6f573230a42550ee9b953806bc3
soif <wxopwx@gmail.com>
2020-12-10 18:57:14 +0100

    Bump Version to 2.0.0a

--- EspTeleInfo/EspTeleInfo.h
+++ EspTeleInfo/EspTeleInfo.h
@@ -54,7 +54,7 @@ void floggerflush();
 #define DEBUG_SERIAL	Serial1
 #define DEBUG_SERIAL1	
 
-#define ESPTELEINFO_VERSION "1.0.2"
+#define ESPTELEINFO_VERSION "2.0.0a"
 
 // I prefix debug macro to be sure to use specific for THIS library
 // debugging, this should not interfere with main sketch or other 
